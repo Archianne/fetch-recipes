d476bf231c7b4170b74e3d58218f176509b196a8
Archianne <helena19w@gmail.com>
2021-05-12 00:18:26 +0100

    other apis added

--- main.js
+++ main.js
@@ -1,60 +1,108 @@
-const searchForm = document.querySelector("form");
-const input = document.querySelector("#foodInput");
-const searchResult = document.querySelector(".searchResult");
+const searchResult = document.querySelector(".search-result");
 const container = document.querySelector(".container");
+const button = document.querySelector("#recipe-button");
+const input = document.querySelector("#food-input");
 let foodToSearch = null;
-
 //important
-const APP_ID = "acf6c552";
-const APP_key = "382791db90157c92aaabdea59f48f996";
+const APP_ID = "c1d6ad2a";
+const APP_key = "0e527f7e2659b24d39614a3dc27b79e6";
 
-//addEventListener
-searchForm.addEventListener("submit", handleRecipeClick);
-
-function handleRecipeClick(event) {
-  event.preventDefault();
-  foodToSearch = input.value;
-  input.value = "";
-  fetchRecipe();
+function handleRecipeClick() {
+  searchResult.innerHTML = "";
+  fetchRecipe(foodToSearch);
 }
 
+function handleFoodChange() {
+  foodToSearch = input.value;
+  input.value = "";
+}
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+});
+
 //async function
-async function fetchRecipe() {
-  const requestUrl = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=18`;
+async function fetchRecipe(food) {
+  foodToSearch = food;
+  const requestUrl = `https://api.edamam.com/search?q=${foodToSearch}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=9`;
   const response = await fetch(requestUrl);
   const data = await response.json();
   handleFood(data.hits);
 }
 
 function handleFood(results) {
-  container.classList.remove("initial");
-  let foodList = "";
-  results.map((foodResult) => {
-    foodList += `
+  results.map((foodToSearch) => {
+    let result = foodToSearch.recipe;
+    searchResult.innerHTML += `
     <div class="items">
-    <a target="_blank" href="${foodResult.recipe.url}"><img src="${
-      foodResult.recipe.image
-    }" alt="${foodResult.recipe.label}"></a>
+      <a target="_blank" href="${result.url}"><img src="${
+        result.image
+      }" alt="${result.label}"></a>
     <div class="resultsDisplay">
-    <h2 class="title">${foodResult.recipe.label}</h2>
-    <a class="viewLink" target="_blank" href="${
-      foodResult.recipe.url
-    }">View Recipe</a>
-    </p>
+      <h2 class="title">${result.label}</h2>
+      <a class="viewLink" target="_blank" href="${
+        result.url
+      }">View Recipe</a>
+    
     </div>
-      <p class="itemInfo">Number of servings: ${foodResult.recipe.yield}
-    <p class="itemInfo">Calories: ${foodResult.recipe.calories.toFixed(2)}
-    </p>
-    <p class="itemInfo">Diet label: ${
-      foodResult.recipe.dietLabels.length > 0
-        ? foodResult.recipe.dietLabels
+      <p class="itemInfo">Number of servings: ${result.yield}</p>
+      <p class="itemInfo">${result.cuisineType}</p>
+      <p class="itemInfo">Calories: ${result.calories.toFixed(0)}</p>
+      <p class="itemInfo">Diet label: ${
+        result.dietLabels.length > 0
+        ? result.dietLabels
         : "Not found"
-    } 
-    </p>
+    } </p>
     </div>
     `;
   });
+}
+
+//other API's
+const backgroundCover = document.querySelector(".background-cover");
+const cocktailResult = document.querySelector(".cocktail-result");
+
+async function getRandomPicture() {
+  const url = "https://foodish-api.herokuapp.com/api";
+  const response = await fetch(url);
+  const picData = await response.json();
+  backgroundCover.style.backgroundImage = `url(${picData.image})`;
+}
+getRandomPicture();
+
+async function randomCocktail() {
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+  const response = await fetch(url);
+  const cocktailData = await response.json();
+  const cocktail = cocktailData.drinks[0];
   
-  //add result to html
-  searchResult.innerHTML = foodList;
+  cocktailResult.innerHTML = `
+  <h2 id="cocktail-title">${cocktail.strDrink}</h2>
+  <img id="cocktail-picture" src="${cocktail.strDrinkThumb}">
+  <ul>
+  </ul>
+  `;
+
+  //get ingredients
+  cocktailIngredients = document.querySelector("ul");
+  const getIngredients = Object.keys(cocktail)
+     .filter(function (ingredient) {
+       return ingredient.indexOf("strIngredient") == 0;
+     })
+     .reduce(function (ingredients, ingredient) {
+       if (cocktail[ingredient] != null) {
+         ingredients[ingredient] = cocktail[ingredient];
+       }
+       return ingredients;
+     }, {});
+  
+   for (let key in getIngredients) {
+     let value = getIngredients[key];
+     listIngredients = document.createElement("li");
+     listIngredients.innerHTML = value;
+     cocktailIngredients.appendChild(listIngredients);
+   }
 }
